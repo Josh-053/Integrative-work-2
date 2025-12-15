a9194af6cb18b373cd0245e6df0c56cdb3c281ab
Ali Kazim <154244144+ali-kazim@users.noreply.github.com>
2025-12-15 11:49:26 +0100

    calculated fraction unbound

--- Simulation/run03.cpp
+++ Simulation/run03.cpp
@@ -1,12 +1,12 @@
 $PARAM
-TVCL = 8.31,    // L/h
-TVV1 = 56.1,   // L 
-TVV2 = 44,  // 1/h
+TVCL = 8.31,      // L/h
+TVV1 = 56.1,      // L 
+TVV2 = 44,        // 1/h
 TVQ  = 20.6
 TVKd = 0.0414
 TVn  = 0.84
 
-$CMT CENT PERIPH// 2-compartmental model
+$CMT CENT PERIPH  // 2-compartmental model
 
 $INPUT eGFR = 100 // covariate which has effect on PK parameters
 
@@ -15,10 +15,10 @@ double eGFREffCL = 1.59 ; // eGFR effect on CL
 
 double CL  = TVCL * pow((eGFR/90),eGFREffCL) * exp(ETA(1)); // power function for scaling
 double V1  = TVV1 * exp(ETA(2)) ;
-double V2  = TVV2 ;
-double Q   = TVQ  ;
-double Kd  = TVKd ;
-double n   = TVn  ;
+double V2  = TVV2  ;
+double Q   = TVQ   ;
+double Kd  = TVKd  ;
+double n   = TVn   ;
 
 double k10 = CL/V1 ;
 double k12 = Q/V1  ;
